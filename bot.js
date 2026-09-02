0767439757dd77f9b362ee7cdb239e13369793da
gautamjat64 <gautamjat6400@gmail.com>
2026-09-03 01:56:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,15 @@ const bot = new TelegramBot(token, {polling:true});
 const app = express();
 app.get('/',(req,res)=>res.send('Bot is Live!'));
 app.listen(process.env.PORT||3000);
-bot.onText(/\/start/,(msg)=>{
-  bot.sendMessage(msg.chat.id,`Hello ${msg.from.first_name}! Bot is Live`);
-});
+
 bot.on('message',(msg)=>{
-  if(msg.text!=='/start') bot.sendMessage(msg.chat.id,`You said: ${msg.text}`);
+  bot.sendMessage(msg.chat.id,
+`💖 Hi, ARADHYA HERE 💖
+
+📹 VIDEO CALL SERVICE AVAILABLE 📹
+
+👉 INTERESTED?
+
+✨ REPLY          YES ✨`
+  );
 });
